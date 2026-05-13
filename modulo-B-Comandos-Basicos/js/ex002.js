56c69b58e8febf46869ebcdeb1e79495967bded4
Rogério Neves dos Santos <santosneves86@gmail.com>
2026-05-13 16:42:31 -0300

    13/05/2026

---
+++ modulo-B-Comandos-Basicos/js/ex002.js
@@ -0,0 +1,25 @@
+/*              1º FORMA
+var num1 = Number.parseInt(window.prompt("Digite um número: "))
+var num2 = Number.parseInt(window.prompt("Digite outro número: "))
+
+var soma = num1 + num2
+
+window.alert("A soma dos números é: " + soma)*/
+
+/*              2º FORMA  conversão number -> string
+var num1 = Number(window.prompt("Digite um número: "))
+var num2 = Number(window.prompt("Digite outro número: "))
+
+var soma = num1 + num2
+
+window.alert("A soma dos números é: " + soma)
+*/
+
+/*           Conversão string -> number*/
+var num1 = Number(window.prompt("Digite um número: "))
+var num2 = Number(window.prompt("Digite outro número: "))
+
+var soma = num1 + num2
+window.alert(`A soma entre ${num1} e ${num2} é = ${soma}`)
+
+//window.alert("A soma dos números é: " + soma.toString())
---
+++ modulo-B-Comandos-Basicos/js/ex003.js
@@ -0,0 +1,4 @@
+var nome = window.prompt("Qual seu nome?")
+document.write(`Seu nome ${nome} tem ${nome.length} letras.<br> `)
+document.write(`Seu nome em maiúsculo é ${nome.toUpperCase()} <br>`)
+document.write(`Seu nome em minúsculo é ${nome.toLowerCase()}`)
