71dfcc180639ccdbebf9b25f794e04d7637f35f6
Rogério Neves dos Santos <santosneves86@gmail.com>
2026-05-14 15:10:16 -0300

    14/05/2026

--- modulo-B-Comandos-Basicos/js/ex003.js
+++ modulo-B-Comandos-Basicos/js/ex003.js
@@ -1,4 +1,6 @@
 var nome = window.prompt("Qual seu nome?")
 document.write(`Seu nome ${nome} tem ${nome.length} letras.<br> `)
-document.write(`Seu nome em maiúsculo é ${nome.toUpperCase()} <br>`)
+document.write(`Seu nome em maiúsculo é ${nome.toUpperCase()} <br> `)
 document.write(`Seu nome em minúsculo é ${nome.toLowerCase()}`)
+
+
