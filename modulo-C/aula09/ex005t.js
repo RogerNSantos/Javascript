d0ded39e9891aee37e95a8f984dcec9cd24fafd8
Rogério Neves dos Santos <santosneves86@gmail.com>
2026-05-22 10:25:28 -0300

    22/05/2026

---
+++ modulo-C/aula09/ex005t.js
@@ -0,0 +1,19 @@
+var p1 = window.document.getElementsByTagName('p')[1]
+var corpo = window.document.body
+//var d = window.document.getElementById('msg')
+var d = window.document.querySelector('div#msg')
+//window.document.write('Está escrito assim: '+ p1.innerText)
+
+//p1.style.color = 'red'
+//corpo.style.backgroundColor = 'blue'
+//window.document.write(p1.innerHTML)
+//window.alert(p1.innerText)
+
+//d.style.background = 'black'
+//d.innerText = 'Teste'
+//window.document.getElementById('msg').innerText = 'Olá'
+
+//var d = window.document.getElementsByName('msg')[0]
+//d.innerText = 'AQUI'
+
+d.style.color = 'red'
