e4444cec5bb975f7e3d11fe1cddb2f6633d41474
Rogério Neves dos Santos <santosneves86@gmail.com>
2026-05-28 10:31:08 -0300

    Exercicio finalizado

---
+++ modulo-D/aula12/exercicios/modelo/script.js
@@ -0,0 +1,25 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+
+    var hora = 20
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora < 12){
+        //BOM DIA
+         img.src = 'imagens/manha.png'
+         document.body.style.background = '#899da9'
+    }else if(hora >= 12 && hora < 18){
+        //BOA TARDE
+        img.src = 'imagens/tarde.png'
+         document.body.style.background= '#fdb000'
+    }else{
+        //BOA NOITE
+        img.src = 'imagens/noite.png'
+        document.body.style.background= '#382c20'
+    }
+        
+       
+}
