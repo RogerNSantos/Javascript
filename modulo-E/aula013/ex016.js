e61fbc6f686805f385a8a10c38a5a1ba8b75950b
Rogério Neves dos Santos <santosneves86@gmail.com>
2026-06-02 09:34:17 -0300

    Módulo E ex016

---
+++ modulo-E/aula013/ex016.js
@@ -0,0 +1,16 @@
+ /*var cont = 20
+
+ while (cont > 0) {
+    console.log(`Contador: ${cont}`)
+    cont-- // cont = cont + 1
+ }
+ */
+
+var cont = 1
+do{
+    console.log(`Posição -> ${cont}`)
+    cont++ // cont = cont + 1
+
+}while (cont <= 8) 
+ 
+   
